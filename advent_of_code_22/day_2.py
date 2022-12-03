28c89f3f720ac940a551d69e356e687880625521
nick426 <65999152+nick426@users.noreply.github.com>
2022-12-03 17:43:04 +1100

    Update day_2.py

--- advent_of_code_22/day_2.py
+++ advent_of_code_22/day_2.py
@@ -1,5 +1,4 @@
 # https://adventofcode.com/2022/day/2
-# Raw input file: https://raw.githubusercontent.com/nick426/hobby/master/advent%20of%20code%202022/input_day_2
 # Part 1: What would your total score be if everything goes exactly according to your strategy guide?
 # Part 2: Following the Elf's instructions for the second column, what would your total score be if everything goes
 # exactly according to your strategy guide?
