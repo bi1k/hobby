e9ad1cc420c88f64af4d7226c9c10fba010395dd
nick426 <65999152+nick426@users.noreply.github.com>
2022-12-03 17:42:49 +1100

    Update day_1.py

--- advent_of_code_22/day_1.py
+++ advent_of_code_22/day_1.py
@@ -1,5 +1,4 @@
 # https://adventofcode.com/2022/day/1
-# Raw input file: https://raw.githubusercontent.com/nick426/hobby/master/advent%20of%20code%202022/input_day_1
 # Part 1: Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 # Part 2: Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
 
